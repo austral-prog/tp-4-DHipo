762cb2a260bd7ede5f01bdcd034d4f320844f5a1
DHipo <104331457+DHipo@users.noreply.github.com>
2024-04-12 12:56:40 -0300

    add exercises

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,3 @@
 def leap_year():
-    print("TO DO")
+    year = int(input("Ingrese un año: "))    
+    print(f"El año {year} {'' if (not year % 400) or (not year % 4 and year % 100) else 'no '}es bisiesto")
--- line.py
+++ line.py
@@ -1,2 +1,25 @@
+from math import sqrt
+
 def line():
-    print("TO DO")
+    coef = {
+        "A": 0,
+        "B": 0,
+        "X1": 0,
+        "X2": 0
+    }
+
+    for key, value in coef.items():
+       coef[key] = float(input(f"Ingrese el coeficiente {key}: "))
+
+    for key, value in coef.items():
+        print(f"El coeficiente {key} de su ecuación de la recta es: {value}")
+    
+    print(f"\nPara la siguiente ecuación:\n\tY = " + str(coef["A"]) +"X + " + str(coef["B"]) + "\n")
+    
+    Ypts = [
+        coef['A'] * coef["X1"] + coef["B"],
+        coef['A'] * coef["X2"] + coef["B"]
+    ]
+    
+    print(f"Dados los siguientes puntos:\n\tP1 (" + str(coef["X1"]) + ", " + str(Ypts[0]) + ")\n\tP2 (" + str(coef['X2']) +", " + str(Ypts[1]) + ")")
+    print(f"\nLa distancia entre ellos es: {sqrt((coef['X1'] - coef['X2'])**2 + (Ypts[0] - Ypts[1])**2)}")
